00728dadd2c2bb66cbcb82421527813a13cde7de
Blair Anderson <Blair81@gmail.com>
2017-09-11 16:51:32 -0700

    update build

--- dist/reviews.js
+++ dist/reviews.js
@@ -4,19 +4,37 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 var parse = exports.parse = function parse($) {
-  var rating = $('#acrPopover').attr('title');
-  var ratingAverage = parseFloat(rating.replace(/,/g, ''));
-  // for some reason this is duplicated.
-  var textRaw = $('#acrCustomerReviewLink #acrCustomerReviewText').text().trim();
+  var ratingContainer = $('#acrPopover').attr('title');
+  var hasAnyReviews = ratingContainer && typeof ratingContainer.replace === 'function';
 
-  // "675 customer reviews"
-  var text = textRaw.slice(0, textRaw.length / 2);
-  var count = parseInt(text.replace(/,/g, ''));
+  if (hasAnyReviews) {
+    var rating = ratingContainer;
+    var ratingAverage = parseFloat(rating.replace(/,/g, ''));
+    // for some reason this is duplicated.
+    var textRaw = $('#acrCustomerReviewLink #acrCustomerReviewText').text().trim();
 
-  return {
-    text: text,
-    count: count,
-    rating: rating,
-    ratingAverage: ratingAverage
-  };
+    // "675 customer reviews"
+    var text = textRaw.slice(0, textRaw.length / 2);
+    var count = parseInt(text.replace(/,/g, ''));
+    return {
+      text: text,
+      count: count,
+      rating: rating,
+      ratingAverage: ratingAverage
+    };
+  } else {
+    return {
+      text: 'Zero Customer Reviews',
+      count: 0,
+      rating: 'Not Applicable',
+      ratingAverage: 0.0
+    };
+  }
+};
+
+var expectation = exports.expectation = {
+  count: 0,
+  rating: 'Not Applicable',
+  ratingAverage: 0,
+  text: 'Zero Customer Reviews'
 };
