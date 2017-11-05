bfe4acf0f318d3e3406f1ff272ec3aec3313677f
Blair Anderson <Blair81@gmail.com>
2017-11-04 17:24:29 -0700

    publish

--- dist/images.js
+++ dist/images.js
@@ -6,73 +6,78 @@ Object.defineProperty(exports, "__esModule", {
 var lodash = require('lodash');
 
 var parse = exports.parse = function parse($) {
+  var mainSplitter = "ImageBlockATF', function (A) {";
+  // P.when("ImageBlockATF").execute(function(imblock){
+  // debugger
+  // })
   var str = $.html();
   // return early if these things are not present
-  if (str.indexOf('ImageBlockATF') === -1 || str.indexOf("A.trigger('P.AboveTheFold')") === -1) {
+  if (str.indexOf(mainSplitter) === -1) {
     return {
       images: { count: 0, thumbnails: [] },
       videos: { count: 0, thumbnails: [] }
     };
   }
 
-  str.split('ImageBlockATF')[1].split("A.trigger('P.AboveTheFold')")[0].split('var data = ')[1].split(';')[0];
+  str = str.split(mainSplitter)[1].split('var data =')[1].split(';')[0];
 
+  // console.log(str);
   // https://stackoverflow.com/questions/9036429/convert-object-string-to-json
   var matches = eval('(' + str + ')');
+
   var images = matches['colorImages']['initial'];
   var thumbnails = lodash.map(images, function (image) {
     return lodash.pick(image, ['hiRes', 'large', 'lowRes', 'thumb', 'variant']);
   });
   var count = images.length;
 
-  return {
+  var resp = {
     images: { count: count, thumbnails: thumbnails },
     videos: { count: parseInt(matches['totalVideoCount']), thumbnails: [] }
   };
+  // console.log(JSON.stringify(resp, null, 4));
+  return resp;
 };
 
 var expectation = exports.expectation = {
   'images.html': {
     images: {
-      count: 6,
+      count: 5,
       thumbnails: [{
-        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/61eRbqyBvnL._SL1500_.jpg',
-        large: 'https://images-na.ssl-images-amazon.com/images/I/41JmjikNbUL.jpg',
+        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/51I07uM0%2BuL._SL1000_.jpg',
+        large: 'https://images-na.ssl-images-amazon.com/images/I/41LaA9qBXnL.jpg',
         lowRes: null,
-        thumb: 'https://images-na.ssl-images-amazon.com/images/I/41JmjikNbUL._SS40_.jpg',
+        thumb: 'https://images-na.ssl-images-amazon.com/images/I/41LaA9qBXnL._SS40_.jpg',
         variant: 'MAIN'
       }, {
-        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/713%2BexicbML._SL1500_.jpg',
-        large: 'https://images-na.ssl-images-amazon.com/images/I/41CZHfFVyhL.jpg',
+        hiRes: null,
+        large: 'https://images-na.ssl-images-amazon.com/images/I/51AXcXeTs7L.jpg',
         lowRes: null,
-        thumb: 'https://images-na.ssl-images-amazon.com/images/I/41CZHfFVyhL._SS40_.jpg',
+        thumb: 'https://images-na.ssl-images-amazon.com/images/I/51AXcXeTs7L._SS40_.jpg',
         variant: 'PT01'
       }, {
-        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/81-B1EE5RbL._SL1500_.jpg',
-        large: 'https://images-na.ssl-images-amazon.com/images/I/51Ruaz7fcSL.jpg',
+        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/616ufAUr09L._SL1244_.jpg',
+        large: 'https://images-na.ssl-images-amazon.com/images/I/41LxikBasEL.jpg',
         lowRes: null,
-        thumb: 'https://images-na.ssl-images-amazon.com/images/I/51Ruaz7fcSL._SS40_.jpg',
+        thumb: 'https://images-na.ssl-images-amazon.com/images/I/41LxikBasEL._SS40_.jpg',
         variant: 'PT02'
       }, {
-        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/71B9dP%2BXTZL._SL1500_.jpg',
-        large: 'https://images-na.ssl-images-amazon.com/images/I/41CHP4vhrTL.jpg',
+        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/61DDMJrXY7L._SL1245_.jpg',
+        large: 'https://images-na.ssl-images-amazon.com/images/I/41IPA6DdnaL.jpg',
         lowRes: null,
-        thumb: 'https://images-na.ssl-images-amazon.com/images/I/41CHP4vhrTL._SS40_.jpg',
+        thumb: 'https://images-na.ssl-images-amazon.com/images/I/41IPA6DdnaL._SS40_.jpg',
         variant: 'PT03'
       }, {
-        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/61nJO476LfL._SL1500_.jpg',
-        large: 'https://images-na.ssl-images-amazon.com/images/I/41KcZXTGJfL.jpg',
+        hiRes: 'https://images-na.ssl-images-amazon.com/images/I/71dN0bA%2B1zL._SL1500_.jpg',
+        large: 'https://images-na.ssl-images-amazon.com/images/I/31g10oKfxlL.jpg',
         lowRes: null,
-        thumb: 'https://images-na.ssl-images-amazon.com/images/I/41KcZXTGJfL._SS40_.jpg',
+        thumb: 'https://images-na.ssl-images-amazon.com/images/I/31g10oKfxlL._SS40_.jpg',
         variant: 'PT04'
-      }, {
-        hiRes: null,
-        large: 'https://images-na.ssl-images-amazon.com/images/I/31Dz3JGAVFL.jpg',
-        lowRes: null,
-        thumb: 'https://images-na.ssl-images-amazon.com/images/I/31Dz3JGAVFL._SS40_.jpg',
-        variant: 'PT05'
       }]
     },
-    videos: { count: 0, thumbnails: [] }
+    videos: {
+      count: 0,
+      thumbnails: []
+    }
   }
 };
